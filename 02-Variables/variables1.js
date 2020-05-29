e0de970b35135cacca5674ce8c90e7d1366586f5
Manjunath Koralli <mskoralli@gmail.com>
2020-05-29 23:53:27 +0530

    Variables

---
+++ 02-Variables/variables1.js
@@ -0,0 +1,8 @@
+//let <variable_name>: <data_type> = value;
+var found = true;
+var grade = 8.3;
+var firstName = "Manjunath";
+var lastName = "Koralli";
+console.log(found);
+console.log("Grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
---
+++ 02-Variables/variables1.ts
@@ -0,0 +1,10 @@
+//let <variable_name>: <data_type> = value;
+
+let found:boolean = true;
+let grade:number = 8.3;
+let firstName:string = "Manjunath";
+let lastName:string = "Koralli";
+
+console.log(found);
+console.log("Grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
